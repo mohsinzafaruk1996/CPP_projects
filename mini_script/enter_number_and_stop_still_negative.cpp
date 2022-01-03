71a4bfc1835726d2f3857929a212f5de1f32c497
mohsinzafaruk1996 <33597363+mohsinzafaruk1996@users.noreply.github.com>
2022-01-03 14:44:48 +0000

    add mini cpp script files

---
+++ mini_script/enter_number_and_stop_still_negative.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+       //declaraton of varables
+       int n;
+       do
+       {
+              cout<<"Enter a positve number = ";
+              cin>>n;
+       }
+       while(n>=0);
+       cout<<"You entered a negatve number so program is ended"<<endl;
+       
+    return 0;
+
+}
